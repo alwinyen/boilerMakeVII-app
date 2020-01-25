370ffb629e3614773d6adda5aa59e910dbeb07fb
AlwinYen 顏才翔 <alwin60805@gmail.com>
2020-01-25 03:44:24 -0500

    [add] add react map module

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import '../src/home.css'
+import MapChart from "./MapChart";
+
+function App() {
+  return (
+    <div>
+		<div id = "header">
+			<h1>BoilerMake VII</h1>
+		</div>
+		<div id = "filter">
+			<p id = "regionTitle">Region</p>
+			<p id = "typeTitle">Type</p>
+			<p id = "timeTitle">Time</p>
+			<br/>
+			<select id = "region">
+			  <option value = "america">America</option>
+			  <option value = "asia">Asia</option>
+			  <option value = "africa">Africa</option>
+			  <option value = "australia">Australia</option>
+			  <option value = "antarctica">Antarctica</option>
+			  <option value = "europe">Europe</option>
+			  <option value = "all">All</option>
+			</select>
+			<select id = "type">
+			  <option value = "environment">Environment</option>
+			  <option value = "politic">Politic</option>
+			  <option value = "all">All</option>
+			</select>
+			<select id = "time">
+			  <option value = "week">Past Week</option>
+			  <option value = "month">Past Month</option>
+			  <option value = "year">Past Year</option>
+			  <option value = "all">All</option>
+			</select>
+		</div>
+      <MapChart />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
