d8cee2834c0ef429da90d14302e64182c225a37d
AlwinYen 顏才翔 <alwin60805@gmail.com>
2020-01-25 13:45:22 -0500

    [add] news template

--- src/components/News.js
+++ src/components/News.js
@@ -1,34 +1,53 @@
 import React from "react";
+import { news }  from '../constant/news'
+import {
+    withRouter
+  } from 'react-router-dom'
 class News extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            new: {}
+        };
+    }
+
+    componentDidMount() {
+        
+        var filteredNews = news.filter(newObj => {
+            return newObj.country == this.props.match.params.country
+        })
+
+        this.setState({new: filteredNews[0]})
+
+    }
     render() {
         return(
             <React.Fragment>
                 <div id = "content1">
-                    <img src = "../../australia wildfire.jpg"></img>
+                    <img src = {this.state.new.imgURL}></img>
                     <div id = "text">
-                        <h1>Australia Bushfire</h1>
+                        <h1>{this.state.new.title}</h1>
                         <div id = "subtitle">
                             <strong>
-                                The deadly wildfires, fanned by wind and fueled by scorching heat, are raging across the southeast of the country.
+                                {this.state.new.subTitle}
                             </strong>
                         </div>
                         <p>
-                            Right now, Australia is being ravaged by one of the most devastating bushfire seasons the country has ever seen.
-                            <br/><br/>
-                            So far, more than 10 million hectares of Australian land has been burned. <br/> <br/>
-                            It’s estimated that as many as 8,400 koalas have perished in fires on the mid-north coast of NSW and Kangaroo Island in South Australia has lost over 50% of their koala population This is a devastating blow for an iconic species already in decline, and these bushfires have the potential to hasten koalas’ slide towards extinction in eastern Australia. <br/> <br/>
-    >                        Koalas could be extinct in the wild in eastern Australia in as little as 30 years due mainly to ongoing excessive tree-clearing for agricultural and urban development and climate heating triggered bushfires that destroy koala habitat, that potentially accelerate the extinction timeline.
+                        {this.state.new.content1} <br/> <br/>
+                        {this.state.new.content2} <br/> <br/>
+                        {this.state.new.content3}
                         </p>
                     </div>
                 </div>
                 <div id = "content2">
                     <h1>How can I help?</h1>
-                    <a href = "https://www.savethekoala.com/donate" target="_blank">Donate to Save Koala!</a>
-                    <a href = "https://www.redcross.org.au/get-involved/connect/volunteer" target="_blank">Volunteer With Australian Red Cross!</a>
+                    <a href = {this.state.new.donateURL} target="_blank">{this.state.new.donateText}</a>
+                    <a href = {this.state.new.helpURL} target="_blank">{this.state.new.helpText}</a>
                 </div>
             </React.Fragment>
         )
     }
 }
 
-export default News;
+export default withRouter(News);
---
+++ src/constant/news.js
@@ -0,0 +1,15 @@
+export const news = [
+    {
+        country: "Australia",
+        title: "Australia Bushfire",
+        imgURL: "https://storage.googleapis.com/boilermakevii.appspot.com/australia%20wildfire.jpg",
+        subTitle: "The deadly wildfires, fanned by wind and fueled by scorching heat, are raging across the southeast of the country.",
+        content1: "Right now, Australia is being ravaged by one of the most devastating bushfire seasons the country has ever seen. So far, more than 10 million hectares of Australian land has been burned.",
+        content2: "It’s estimated that as many as 8,400 koalas have perished in fires on the mid-north coast of NSW and Kangaroo Island in South Australia has lost over 50% of their koala population This is a devastating blow for an iconic species already in decline, and these bushfires have the potential to hasten koalas’ slide towards extinction in eastern Australia.",
+        content3: "Koalas could be extinct in the wild in eastern Australia in as little as 30 years due mainly to ongoing excessive tree-clearing for agricultural and urban development and climate heating triggered bushfires that destroy koala habitat, that potentially accelerate the extinction timeline.",
+        donateURL: "https://www.savethekoala.com/donate",
+        donateText: "Donate to Save Koala!",
+        helpURL: "https://www.redcross.org.au/get-involved/connect/volunteer",
+        helpText: "Volunteer With Australian Red Cross!"
+    }
+]
