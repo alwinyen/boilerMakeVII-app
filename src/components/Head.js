085d819c018d0416a1d716f685cf106c145f8fe0
AlwinYen 顏才翔 <alwin60805@gmail.com>
2020-01-25 08:05:39 -0500

    [add] add compoments

---
+++ src/components/Head.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Head extends React.Component {
+    render() {
+        return(
+            <div id = "header">
+			    <h1>BoilerMake VII</h1>
+		    </div>
+        )
+    }
+}
+
+export default Head;
--- src/index.js
+++ src/index.js
@@ -3,45 +3,44 @@ import ReactDOM from "react-dom";
 import '../src/home.css'
 import MapChart from "./MapChart";
 import ReactTooltip from "react-tooltip";
+import Head from './components/Head'
 
 function App() {
   const [content, setContent] = useState("");
   return (
-    <div>
-		<div id = "header">
-			<h1>BoilerMake VII</h1>
-		</div>
-		<div id = "filter">
-			<p id = "regionTitle">Region</p>
-			<p id = "typeTitle">Type</p>
-			<p id = "timeTitle">Time</p>
-			<br/>
-			<select id = "region">
-			  <option value = "america">America</option>
-			  <option value = "asia">Asia</option>
-			  <option value = "africa">Africa</option>
-			  <option value = "australia">Australia</option>
-			  <option value = "antarctica">Antarctica</option>
-			  <option value = "europe">Europe</option>
-			  <option value = "all">All</option>
-			</select>
-			<select id = "type">
-			  <option value = "environment">Environment</option>
-			  <option value = "politic">Politic</option>
-			  <option value = "all">All</option>
-			</select>
-			<select id = "time">
-			  <option value = "week">Past Week</option>
-			  <option value = "month">Past Month</option>
-			  <option value = "year">Past Year</option>
-			  <option value = "all">All</option>
-			</select>
-		</div>
-    <div id = "map">
-      <MapChart setTooltipContent={setContent} />
-      <ReactTooltip>{content}</ReactTooltip>
-    </div>
-    </div>
+    <React.Fragment>
+      <Head/>
+      <div id = "filter">
+        <p id = "regionTitle">Region</p>
+        <p id = "typeTitle">Type</p>
+        <p id = "timeTitle">Time</p>
+        <br/>
+        <select id = "region">
+          <option value = "america">America</option>
+          <option value = "asia">Asia</option>
+          <option value = "africa">Africa</option>
+          <option value = "australia">Australia</option>
+          <option value = "antarctica">Antarctica</option>
+          <option value = "europe">Europe</option>
+          <option value = "all">All</option>
+        </select>
+        <select id = "type">
+          <option value = "environment">Environment</option>
+          <option value = "politic">Politic</option>
+          <option value = "all">All</option>
+        </select>
+        <select id = "time">
+          <option value = "week">Past Week</option>
+          <option value = "month">Past Month</option>
+          <option value = "year">Past Year</option>
+          <option value = "all">All</option>
+        </select>
+      </div>
+      <div id = "map">
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
+      </div>
+    </React.Fragment>
   );
 }
 
